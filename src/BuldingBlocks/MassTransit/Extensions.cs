1ab009a7ed96ed90c12f950ac45a183fd80d99fd
meysamhadeli <meysamhadeli@gmail.com>
2022-02-08 02:07:38 +0330

    add cap to building block

--- src/BuldingBlocks/MassTransit/Extensions.cs
+++ src/BuldingBlocks/MassTransit/Extensions.cs
@@ -1,10 +1,9 @@
 using System.Reflection;
 using BuildingBlocks.Domain;
 using BuildingBlocks.Utils;
-using Humanizer;
 using MassTransit;
-using MassTransit.Topology;
 using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
 
 namespace BuildingBlocks.MassTransit;
 
@@ -16,72 +15,50 @@ public static class Extensions
         {
             configure.AddConsumers(Assembly.GetEntryAssembly());
 
-            configure.UsingRabbitMq(
-                (context, configurator) =>
+            configure.UsingRabbitMq((context, configurator) =>
+            {
+                var rabbitMqOptions = services.GetOptions<RabbitMqOptions>("RabbitMq");
+                configurator.Host(rabbitMqOptions.HostName, h =>
                 {
-                    var rabbitMqOptions = services.GetOptions<RabbitMqOptions>("RabbitMq");
-                    configurator.Host(rabbitMqOptions.HostName, h =>
-                    {
-                        h.Username(rabbitMqOptions.UserName);
-                        h.Password(rabbitMqOptions.Password);
-                    });
-
-                    var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
-                        .Where(x => x.IsAssignableTo(typeof(IEvent)) && x.IsInterface == false &&
-                                    x.IsAbstract == false &&
-                                    x.IsGenericType == false);
-
-
-                    foreach (var type in types)
-                    {
-                        var consumers = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
-                            .Where(x => x.IsAssignableTo(typeof(IConsumer<>).MakeGenericType(type))).ToList();
-
-                        //////////
-                        var messageMethodInfo = configurator.GetType().GetMethod("Message");
-                        var messageMethodInfoGeneric = messageMethodInfo?.MakeGenericMethod(type);
-                        var messageTopologyMethodType = typeof(Extensions).GetMethods()
-                            .FirstOrDefault(x => x.Name == nameof(CreateMessageTopologyConfigurator))
-                            ?.MakeGenericMethod(type);
-
-                        var messageMethodDelegate = messageTopologyMethodType?.Invoke(null, null);
-
-                        // prepare delegate instance
-                        messageMethodInfoGeneric?.Invoke(configurator, new[]
-                        {
-                            messageMethodDelegate
-                        });
-
-                        configurator.ReceiveEndpoint(
-                            string.IsNullOrEmpty(rabbitMqOptions.ExchangeName)
-                                ? type.Name.Underscore()
-                                : $"{rabbitMqOptions.ExchangeName}_{type.Name.Underscore()}", e =>
-                            {
-                                foreach (var consumer in consumers)
-                                {
-                                    configurator.ConfigureEndpoints(context, x => x.Exclude(consumer));
-
-                                    var methodInfo = typeof(DependencyInjectionReceiveEndpointExtensions)
-                                        .GetMethods()
-                                        .Where(x => x.GetParameters()
-                                            .Any(p => p.ParameterType == typeof(IServiceProvider)))
-                                        .FirstOrDefault(x => x.Name == "Consumer" && x.IsGenericMethod);
-
-                                    var generic = methodInfo?.MakeGenericMethod(consumer);
-                                    generic?.Invoke(e, new object[] {e, context, null});
-                                }
-                            });
-                    }
+                    h.Username(rabbitMqOptions.UserName);
+                    h.Password(rabbitMqOptions.Password);
                 });
+
+                var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
+                    .Where(x => x.IsAssignableTo(typeof(IEvent)) && x.IsInterface == false &&
+                                x.IsAbstract == false &&
+                                x.IsGenericType == false);
+
+                foreach (var type in types)
+                {
+                    var consumers = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
+                        .Where(x => x.IsAssignableTo(typeof(IConsumer<>).MakeGenericType(type))).ToList();
+
+                    if (consumers.Any())
+                        configurator.ReceiveEndpoint(rabbitMqOptions.ExchangeName, e =>
+                        {
+                            foreach (var consumer in consumers)
+                            {
+                                configurator.ConfigureEndpoints(context, x => x.Exclude(consumer));
+
+                                var methodInfo = typeof(DependencyInjectionReceiveEndpointExtensions)
+                                    .GetMethods()
+                                    .Where(x => x.GetParameters()
+                                        .Any(p => p.ParameterType == typeof(IServiceProvider)))
+                                    .FirstOrDefault(x => x.Name == "Consumer" && x.IsGenericMethod);
+
+                                var generic = methodInfo?.MakeGenericMethod(consumer);
+                                generic?.Invoke(e, new object[] {e, context, null});
+                            }
+
+                            e.ExchangeType = ExchangeType.Topic;
+                        });
+                }
+            });
         });
 
         services.AddMassTransitHostedService();
 
         return services;
-    }
-
-    public static Action<IMessageTopologyConfigurator<T>> CreateMessageTopologyConfigurator<T>() where T : class
-    {
-        return param => { param.SetEntityName(typeof(T).Namespace.Underscore() + "_" + typeof(T).Name.Underscore()); };
     }
 }
--- src/Services/Airline.Identity/src/Identity.Api/Consumers/FlightCreated.cs
+++ src/Services/Airline.Identity/src/Identity.Api/Consumers/FlightCreated.cs
@@ -1,5 +1,4 @@
 using BuildingBlocks.Domain;
 
 namespace Contracts.Event;
-
 public record FlightCreated(string FlightNumber) : IEvent;
